8be53940f4a11b8040820bdc8469b921f04dc397
Slavcho Mitrov <mitrov.slavcho@gmail.com>
2026-06-17 16:10:51 +0200

    setup laravel jwt auth and controller with paths

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+// use Illuminate\Http\Request;
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::group([
+    'middleware' => 'api',
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('me', [AuthController::class, 'me']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
+});
